7edab7a9543bc249c53a92180dec8120f3dbfdb3
Boti <medgyesi.me@icloud.com>
2025-04-09 11:19:58 +0200

    created the two model casses and added their fields accoring to table 1

---
+++ src/main/java/model/AppUser.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.time.LocalDate;
+
+public class AppUser {
+
+    private int id;
+    private String name;
+    private String password;
+    private LocalDate regDate;
+    private Details userDetails;
+
+    
+}
---
+++ src/main/java/model/Details.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Details {
+
+    private int id;
+    private String email;
+    private String name;
+    private LocalDate birthday;
+
+}
